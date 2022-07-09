e70f5f9e2f0078e259eefd5b8e9d877bdd1c13c5
cmessere <carlo.messere@accenture.com>
2022-07-10 01:36:14 +0200

    added fetch for users and organizations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import * as React from 'react';
 import './App.css';
+import { usersApi, organizationsApi } from './Utilities/Api';
 
 function App() {
+  const [users, setUsers] = React.useState(undefined as any )
+  const [organizations, setOrganizations] = React.useState(undefined as any )
+
+  React.useEffect(() => {
+
+      usersApi()
+        .then((response: any) => {
+          setUsers(response.data)
+        })
+        .catch((e: any) => console.log(e))
+
+      organizationsApi()
+        .then((response: any) => {
+          setOrganizations(response.data)
+        })
+        .catch((e: any) => console.log(e))
+
+  }, [])
+
+
+
   return (
     <div >
 
