b0a667b8484659f8b4ba80c32e85c7fba566568d
cmessere <carlo.messere@accenture.com>
2022-07-11 00:10:59 +0200

    Added back link on error page

--- src/Pages/ErrorPage.tsx
+++ src/Pages/ErrorPage.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = ({message}) => {
     return(
         <div className="error-page">
             <h1 className="error-title">Ops...Something went wrong 😭</h1>
             <h2 className="error-subtitle">{message}</h2>
+            <Link className='repo-detail-link' to={'/'}>Go back</Link>
         </div>
       )
 }
