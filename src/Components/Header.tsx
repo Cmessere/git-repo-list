fa174a687d5e12849a7dcbd42d7319b8491d9cd3
cmessere <carlo.messere@accenture.com>
2022-07-10 22:44:35 +0200

    created header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faPlaneDeparture } from '@fortawesome/free-solid-svg-icons'
+import { useHistory } from 'react-router-dom';
 
 const Header = () => {
+    const history = useHistory()
     return(
         <div className='header-container'>
+            <p onClick={() => history.push("/")} className='header-icon'><FontAwesomeIcon icon={faHouse} /></p>
             <h1 className='header-title'>GitHub Search</h1>
         </div>
       )
