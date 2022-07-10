9d255dcb9b7f4c30cfe837d983787e1eb28cf6c6
cmessere <carlo.messere@accenture.com>
2022-07-10 03:06:00 +0200

    using promise all and handling promises branches

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import * as React from 'react';
+import { gitHubClient } from './Utilities/Api';
 import './App.css';
 import { usersApi, organizationsApi } from './Utilities/Api';
 
 function App() {
   const [users, setUsers] = React.useState(undefined as any )
   const [organizations, setOrganizations] = React.useState(undefined as any )
+  const [error, setError] = React.useState({isError:false} as FetchError)
+  const [isLoading, setIsLoading] = React.useState(true)
 
   React.useEffect(() => {
 
-      usersApi()
-        .then((response: any) => {
-          setUsers(response.data)
-        })
-        .catch((e: any) => console.log(e))
-
-      organizationsApi()
-        .then((response: any) => {
-          setOrganizations(response.data)
-        })
-        .catch((e: any) => console.log(e))
+      Promise.all([
+        gitHubClient.get('/users'),
+        gitHubClient.get('/organizations')
+      ])
+      .then(([usersResponse, organizationsResponse])=>{
+        setUsers(usersResponse.data)
+        setOrganizations(organizationsResponse.data)
+      })
+      .catch((e: any) => {
+        setError({isError:true, message:e.message})
+      })
+      .finally(() => setIsLoading(true))
 
   }, [])
 
