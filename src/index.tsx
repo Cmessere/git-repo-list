897351b820e4180a91d927742daa0a05e636ab90
cmessere <carlo.messere@accenture.com>
2022-07-10 19:06:00 +0200

    Added routing

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,16 @@
 import './index.css';
 import App from './App';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import * as React from 'react';
 
 const element = document.getElementById('root');
 const root = createRoot(element!);
 root.render(
-    <App />
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" ><App /></Route>
+            <Route path="/detail" ><h1>Detail</h1></Route>
+        </Switch >
+    </BrowserRouter>
 );
