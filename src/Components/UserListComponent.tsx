614af191d6ab53d23848d40144fea78508dcac6c
cmessere <carlo.messere@accenture.com>
2022-07-10 16:32:53 +0200

    Changed UserList style

--- src/Components/UserListComponent.tsx
+++ src/Components/UserListComponent.tsx
@@ -3,7 +3,7 @@ import { capitalizeString } from '../Utilities/UtilityFunctions';
 
 const UserListComponent = ({users}) => {
     return(
-        <div className="user-list-container">
+        <div>
             <ul className='user-list'>
                 {users.map(u => <li className='user-list-item'><p className='user-list-paragraph'>{capitalizeString(u.login)}</p><img src={u.avatar_url} alt="Avatar" className="avatar"/> </li>)}
             </ul>
--- src/Pages/UserListPage.tsx
+++ src/Pages/UserListPage.tsx
@@ -4,8 +4,13 @@ import UserListComponent from '../Components/UserListComponent';
 const UserListPage = ({users, organizations}) => {
     return(
         <div className='user-list-page'>
-            <UserListComponent users={users} />
-            <UserListComponent users={organizations} />
+            <div className='user-list-title-container'>
+                <h2 className='user-list-title'>Random users</h2>
+            </div>
+            <div className='user-list-container'>
+                <UserListComponent users={users} />
+                <UserListComponent users={organizations} />
+            </div>
         </div>
       )
 }
